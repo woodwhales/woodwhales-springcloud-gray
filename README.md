f33e09d2e973ba8c8f95607c49742b0d7265bceb
woodwhales <woodwhales@163.com>
2023-01-03 20:37:56 +0800

    added springcloud gateway

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 spring-cloud 灰度发布示例
 
-| 服务              | 端口号 |
-| ----------------- | ------ |
-| gray-eureka       | 9079   |
-| gray-zuul         | 9080   |
-| gray-server1-new  | 9083   |
-| gray-server1-old  | 9081   |
-| gray-server2-new1 | 9084   |
-| gray-server2-new2 | 9085   |
-| gray-server2-old  | 9082   |
+| 服务                | 端口号  |
+|-------------------|------|
+| gray-eureka       | 9079 |
+| gray-zuul         | 9080 |
+| gray-server1-new  | 9083 |
+| gray-server1-old  | 9081 |
+| gray-server2-new1 | 9084 |
+| gray-server2-new2 | 9085 |
+| gray-server2-old  | 9082 |
+| gray-gateway      | 9086 |
---
+++ gray-gateway/src/main/java/cn/woodwhales/gray/gateway/Application.java
@@ -0,0 +1,22 @@
+package cn.woodwhales.gray.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author woodwhales on 2022-12-28 19:57
+ */
+@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication
+@ComponentScan({"cn.woodwhales.gray.**"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
